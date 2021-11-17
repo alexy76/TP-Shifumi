85f5b19643a0075934238fdb5e907f22b89126bd
AlexyL <alexy.lepretre76@laposte.net>
2021-11-17 21:30:36 +0100

    Variables renommée fichier JS, purification dans le html, idem pour le style.css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,15 @@
 const shiFuMi = ["pierre", "ciseaux", "feuille"]
 
-let wordUser = "pierre"
-let nbUser = 0
-let scoreUser = 0
-let scoreIA = 0
-scoreofuser.textContent = `${scoreUser}`
-scoreofia.textContent = `${scoreIA}`
+let wordUser = "pierre", a = 0, scoreUser = 0, scoreIA = 0
 
-
+scoreu.textContent = `${scoreUser}`
+scoreia.textContent = `${scoreIA}`
 
 function setScore(id, user){
 
     if(user == "null"){
         result.innerHTML = '<span class="d-inline-block btn btn-warning text-white">Egalité</span>'
-    }
-    else if(user == "User"){
+    }else if(user == "User"){
         scoreUser++
         id.textContent = `${scoreUser}`
         result.innerHTML = '<span class="d-inline-block btn btn-success text-white">Gagné</span>'
@@ -23,27 +18,24 @@ function setScore(id, user){
         id.textContent = `${scoreIA}`
         result.innerHTML = '<span class="d-inline-block btn btn-danger text-white">Perdu</span>'
     }
+
+    imgUser.src = `assets/img/${wordUser}.png`
+    imgIA.src = `assets/img/${wordIA}.png`
 }
 
 containerForm.addEventListener("click", (e) => {
 
     if(e.target.nodeName == "IMG"){
-
         document.getElementById(`${wordUser}IMG`).src = `assets/img/${wordUser}.png`
         e.target.src = `assets/img/${e.target.alt}Clicked.png`
 
         wordUser = e.target.alt
-        nbUser = e.target.dataset.number
-
+        a = e.target.dataset.number
     }
 
     if(e.target.nodeName == "BUTTON"){
-
-        nbIA = Math.floor(Math.random() * shiFuMi.length)
-        wordIA = shiFuMi[nbIA]
-        nbUser == nbIA ? setScore(null, "null") : (nbUser < nbIA && nbUser == nbIA-1 || nbUser == 2 && nbUser == nbIA+2) ? setScore(scoreofuser, "User") : setScore(scoreofia, "IA")
-
-        imgUser.src = `assets/img/${wordUser}.png`
-        imgIA.src = `assets/img/${wordIA}.png`
+        b = Math.floor(Math.random() * shiFuMi.length)
+        wordIA = shiFuMi[b]
+        a == b ? setScore(null, "null") : a < b && a == b-1 || a == 2 && a == b+2 ? setScore(scoreu, "User") : setScore(scoreia, "IA")
     }
 })
